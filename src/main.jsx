c2d044349a3c9fba52668a0110958677c0dcc665
Nocaterate <mariojovanqa@gmail.com>
2026-09-15 13:32:45 +0700

    Venture Creation

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../wealthify-financial-platform.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
